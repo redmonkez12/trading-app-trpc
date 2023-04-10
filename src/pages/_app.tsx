73bd20fcddd86aeaa256c579e8e070b3cb380a83
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2023-04-10 23:56:35 +0200

    Add warning message

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { getSession, SessionProvider } from "next-auth/react";
+import Image from "next/image";
 
-import { MantineProvider, Group } from "@mantine/core";
+import { MantineProvider, Group, Alert } from "@mantine/core";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
-import { Navigation, type User } from "~/components/Navigation/Navigation";
+import { type User } from "~/components/Navigation/Navigation";
 import { NavigationNew } from "~/components/Navigation/NavigationNew";
 
 const MyApp: AppType<{ session: Session | null }> = ({
@@ -38,6 +39,10 @@ const MyApp: AppType<{ session: Session | null }> = ({
         session={session}
         refetchInterval={5 * 60}
         refetchOnWindowFocus={true}>
+        <Alert icon={<Image src={"/images/danger.svg"} width={20} height={20} alt={"danger"}/>} color="red">
+          This application is under development, please don't use it for real data.
+        </Alert>
+
         <Group align={"flex-start"} className={"p-8 flex-col md:flex-row h-full"}>
           {user && <NavigationNew user={user} />}
           <div className={"flex justify-center w-full flex-1"}>
